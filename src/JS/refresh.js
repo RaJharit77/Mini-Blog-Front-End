171d5e39b684165bd85372f129f0412ebfbc937b
RaJharit77 <hei.rajo.2@gmail.com>
2024-11-09 15:44:40 +0300

    feat: adding new feature

--- src/JS/refresh.js
+++ src/JS/refresh.js
@@ -8,3 +8,13 @@ window.addEventListener('keydown', function (event) {
 window.addEventListener('beforeunload', function (event) {
     // Aucun code à exécuter ici pour éviter un rafraîchissement indésirable
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const refreshButton = document.getElementById('refresh-button');
+
+    if (refreshButton) {
+        refreshButton.addEventListener('click', function () {
+            location.reload();
+        });
+    }
+});
