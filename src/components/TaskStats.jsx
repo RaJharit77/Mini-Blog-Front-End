58da1c04f2d6ece6688ccaa7064efe5c14c20bb9
RaJharit77 <hei.rajo.2@gmail.com>
2024-11-09 17:39:44 +0300

    feat: Update server

--- src/components/TaskStats.jsx
+++ src/components/TaskStats.jsx
@@ -39,33 +39,41 @@ function TaskStats() {
                 Statistiques des tâches
             </h1>
             <div className="flex flex-col sm:flex-row justify-center w-full space-y-4 sm:space-y-0 sm:space-x-4">
-                <div className="w-full sm:w-1/2 p-2 sm:p-4">
-                    <h2 className="text-md sm:text-lg mb-2 bg-gradient-to-r from-yellow-400 via-pink-400 to-sky-400 bg-clip-text text-transparent text-center sm:text-left">Diagramme en Doughnut</h2>
-                    <Doughnut data={chartData} options={{
-                        maintainAspectRatio: true,
-                        responsive: true,
-                        plugins: {
-                            legend: {
-                                labels: {
-                                    color: 'white',
+                <div className="w-full sm:w-1/2 p-2 sm:p-4 flex justify-center">
+                    <div className="w-3/4"> {/* Réduction de la taille du conteneur du Doughnut */}
+                        <h2 className="text-md sm:text-lg mb-2 bg-gradient-to-r from-yellow-400 via-pink-400 to-sky-400 bg-clip-text text-transparent text-center sm:text-left">
+                            Diagramme en Doughnut
+                        </h2>
+                        <Doughnut data={chartData} options={{
+                            maintainAspectRatio: true,
+                            responsive: true,
+                            plugins: {
+                                legend: {
+                                    labels: {
+                                        color: 'white',
+                                    }
                                 }
                             }
-                        }
-                    }} />
+                        }} />
+                    </div>
                 </div>
-                <div className="w-full sm:w-1/2 p-2 sm:p-4 mt-4 sm:mt-0">
-                    <h2 className="text-md sm:text-lg mb-2 bg-gradient-to-r from-sky-500 via-pink-400 to-yellow-400 bg-clip-text text-transparent text-center sm:text-left">Diagramme en Pie</h2>
-                    <Pie data={chartData} options={{
-                        maintainAspectRatio: true,
-                        responsive: true,
-                        plugins: {
-                            legend: {
-                                labels: {
-                                    color: 'white',
+                <div className="w-full sm:w-1/2 p-2 sm:p-4 mt-4 sm:mt-0 flex justify-center">
+                    <div className="w-3/4"> {/* Réduction de la taille du conteneur du Pie */}
+                        <h2 className="text-md sm:text-lg mb-2 bg-gradient-to-r from-sky-500 via-pink-400 to-yellow-400 bg-clip-text text-transparent text-center sm:text-left">
+                            Diagramme en Pie
+                        </h2>
+                        <Pie data={chartData} options={{
+                            maintainAspectRatio: true,
+                            responsive: true,
+                            plugins: {
+                                legend: {
+                                    labels: {
+                                        color: 'white',
+                                    }
                                 }
                             }
-                        }
-                    }} />
+                        }} />
+                    </div>
                 </div>
             </div>
         </div>
